c625fa7372eed9091791a9b1c00ab29aec8f94ec
JODACHSE <jonathandavidchavarrosegura@gmail.com>
2025-05-14 18:51:53 -0500

    feat(presentation): add contact, conclusions, and model engineering pages
    Add new presentation pages for contact, conclusions, and model engineering, including HTML templates, CSS styles, and JavaScript functionality. Enhance developer profiles, contact forms, and interactive card components. Improve layout and responsiveness across all pages.

---
+++ static/js/presentation/conslusiones.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página de conclusiones cargada correctamente');
+    
+    // Aquí puedes agregar funcionalidades específicas para esta página
+});
